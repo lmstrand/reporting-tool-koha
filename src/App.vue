4b6c9bba18c2d543497746200e6bc7be59bb5f53
lmstrand <lmstrand@gmail.com>
2021-11-29 22:23:22 +0200

    filtered exporting as greet

--- src/App.vue
+++ src/App.vue
@@ -36,10 +36,45 @@
 <button type="button" class="btn btn-success">Success</button>
 <b-button @click="toggleBusy">Toggle Busy State</b-button>
 
+  <b-dropdown id="dropdown-divider" text="Kuukausi" class="m-2">
+    <b-dropdown-item-button>Tammikuu</b-dropdown-item-button>
+    <b-dropdown-item-button>Helmikuu</b-dropdown-item-button>
+    <b-dropdown-divider></b-dropdown-divider>
+    <b-dropdown-item-button>Koko vuosi</b-dropdown-item-button>
+  </b-dropdown>
+  
+  <b-dropdown id="dropdown-divider" text="Vuosi" class="m-2">
+    <b-dropdown-item-button>2021</b-dropdown-item-button>
+    <b-dropdown-item-button>2020</b-dropdown-item-button>
+    <b-dropdown-item-button>2019</b-dropdown-item-button>
+  </b-dropdown>
+  
+  <div>
+    <b-form-select v-model="selected" :options="options" size="sm" class="mt-3"></b-form-select>
+    <div class="mt-3">Selected: <strong>{{ selectedMonth }}</strong></div>
+  </div>
+
+
+<b-row class="mb-6">
+<b-col lg="2" class="my-1">
+<b-form-datepicker id="example-datepicker" v-model="lowDate" ></b-form-datepicker>
+</b-col>
+<b-col lg="2" class="my-1">
+<b-form-datepicker id="example-datepicker" v-model="maxDate" ></b-form-datepicker>
+
+
+
+
+</b-col>
+
+
+</b-row>
+
+  
 <b-row>
-<b-col lg="6" class="my-1">
+<b-col lg="4" class="my-1">
         <b-form-group
-          label="Filter"
+          label="Suodata"
           label-for="filter-input"
           label-cols-sm="3"
           label-align-sm="right"
@@ -51,11 +86,11 @@
               id="filter-input"
               v-model="filter"
               type="search"
-              placeholder="Type to Search"
+              placeholder="Etsi..."
             ></b-form-input>
 
             <b-input-group-append>
-              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+              <b-button :disabled="!filter" @click="filter = ''">Tyhjennä</b-button>
             </b-input-group-append>
           </b-input-group>
         </b-form-group>
@@ -63,7 +98,7 @@
  </b-row>     
       
    
-  <b-table striped responsive="sm" bordered outlined small sort-icon-left :fields=fields :items=myJson.data :busy="isBusy"  :filter="filter">
+  <b-table v-model="filtered" striped responsive="sm" bordered outlined small sort-icon-left :fields=fields :items=myJson.data :busy="isBusy"  :filter="filter">
   
   <template #table-busy>
         <div class="text-center text-danger my-2">
@@ -74,17 +109,17 @@
       
       <template #cell(itemtypes)="row">
         <b-button size="sm" @click="row.toggleDetails" class="mr-2">
-          {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
+          {{ row.detailsShowing ? 'Piilota' : 'Näytä'}} tiedot
         </b-button>
 
         <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
-        <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
+  <!--      <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
           Details via check
-        </b-form-checkbox>
+        </b-form-checkbox>  -->
       </template>
 
       <template #row-details="row">
-        <b-table class="w-100 p-3 bordered outlined text-light" striped hover responsive="sm" :items=row.item.itemtypes.data>
+        <b-table class="w-100 p-3 bordered outlined text-light" striped hover responsive="sm" :items=row.item.itemtypes>
         
         
         
@@ -138,7 +173,7 @@ let fields = [
                 { label:"Muut äänitteet", key:"other_regording" , sortable: true},
                 { label:"Videot", key:"videos" , sortable: true},
                 { label:"Yhteensä", key:"total" , sortable: true},
-                { label:"Itemtypes", key:"itemtypes" , sortable: true},
+                { label:"Itemtypes", key:"itemtypes" },
                 
                 
             ];
@@ -161,6 +196,16 @@ let itemtypefields = [
     
     
    return {
+     selectedMonth: null,
+        options: [
+          { value: null, text: 'Please select an option' },
+          { value: 'a', text: 'This is First option' },
+          { value: 'b', text: 'Selected Option' },
+          { value: { C: '3PO' }, text: 'This is an option with object value' },
+          { value: 'd', text: 'This one is disabled', disabled: true }
+        ],
+     lowDate: '',
+     maxDate: '',
      filter: '',
      isBusy: false,
      fields,
@@ -169,21 +214,7 @@ let itemtypefields = [
     };
   },
   
- // define methods under the `methods` object
-  methods: {
-    toggleBusy() {
-        this.isBusy = !this.isBusy
-    },
-    greet: function (event) {
-      // `this` inside methods points to the Vue instance
-      alert('Hello ' + this.name + '!')
-      // `event` is the native DOM event
-      if (event) {
-        alert(event.target.tagName)
-      }
-    },
-    
-        computed: {
+  computed: {
       sortOptions() {
         // Create an options list from our fields
         return this.fields
@@ -193,6 +224,29 @@ let itemtypefields = [
           })
       }
     },
+  
+ // define methods under the `methods` object
+  methods: {
+    toggleBusy() {
+        this.isBusy = !this.isBusy
+    },
+    greet: function (event) {
+      // `this` inside methods points to the Vue instance, filtered table data printing
+
+
+      let moddedData = JSON.stringify(this.filtered)
+      alert('Hello ' + this.filtered  + moddedData + '!')
+      // `event` is the native DOM event
+      if (event) {
+        alert(event.target.tagName)
+      }
+    },
+    
+    showIssues() {
+        this.isBusy = !this.isBusy
+    },
+    
+        
   }
   
   
